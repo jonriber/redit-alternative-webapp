dbf07026a56491c22fbc9e6737325865ed0c30f4
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-22 11:05:24 +0100

    testing commit global config file

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -3,7 +3,17 @@ import { Button } from '@nextui-org/button'
 export default function Home() {
   return (
     <>
+      <h1>Hello NextUI</h1>
+      <div>
+        <p>NextUI is a React UI library with a simple API.</p>
+        <p>It is built on top of Next.js and Tailwind CSS.</p>
+      </div>
+      <div className='container flex flex-col gap-4'>
       <Button>Click me</Button>
+      <Button>Open Modal</Button>
+      <Button>Login</Button>
+
+      </div>
     </>
   )
 }
