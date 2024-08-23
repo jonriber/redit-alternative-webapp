3c74c2d279783269b22cc9e1c72e032fe430c997
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-23 11:43:47 +0100

    updating my server side header component with a client side header auth component that call auth system through browser

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,14 +1,9 @@
-import { Button } from '@nextui-org/button'
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-export default async function Home() {
 
-  const session = await auth();
+export default function Home() {
 
   return (
     <div className='container flex flex-row gap-4'>
-      <Profile />
+      Home Page
     </div>
   )
 }
--- discuss/src/components/header.tsx
+++ discuss/src/components/header.tsx
@@ -5,49 +5,11 @@ import {
   NavbarContent,
   NavbarItem,
   Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
 } from '@nextui-org/react'
-import { auth } from '@/auth';
-import { 
-  signIn, 
-  signOut 
-} from '@/actions';
+import HeaderAuth from './header-auth';
 
-export default async function Header(){
-  const session = await auth();
-  let authContent: React.ReactNode;
-
-  if (session?.user){
-    authContent = <Popover placement="left">
-      <PopoverTrigger>
-        <Avatar src={session.user.image || ""}/>
-      </PopoverTrigger>
-      <PopoverContent>
-        <div className="p-4"> 
-          <form action={signOut}>
-            <Button type="submit" color="secondary" variant="flat">Sign Out</Button>
-          </form>
-        </div>
-      </PopoverContent>
-    </Popover>
-  }else{
-    authContent = <>
-      <NavbarItem>
-        <form action={signIn}>
-          <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
-        </form>
-      </NavbarItem>
-      <NavbarItem>
-        <form action={signIn}>
-          <Button type="submit" color="primary" variant="flat">Sign Up</Button>
-        </form>
-      </NavbarItem>
-    </>
-  }
+export default function Header(){
+  
   return(
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -61,7 +23,7 @@ export default async function Header(){
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify='end'>
-        {authContent}
+        <HeaderAuth />
       </NavbarContent>
     </Navbar>
   )
