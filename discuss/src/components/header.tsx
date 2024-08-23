c05cf1793c5fd3fba00d2ec640800c7df35b3ddd
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-23 10:54:06 +0100

    header first part implementation & testing on local & server actions use server keyword updated

---
+++ discuss/src/components/header.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import {
+  NavbarBrand,
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+} from '@nextui-org/react'
+import { auth } from '@/auth';
+
+export default async function Header(){
+  const session = await auth();
+
+  return(
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Home Page
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input placeholder="Search" />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify='end'>
+        <NavbarItem>
+          {session?.user ? 
+            <div>Signed in</div>
+            :
+            <div>Signed Out</div>
+          }
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
