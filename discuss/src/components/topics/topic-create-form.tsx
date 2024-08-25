31c5f547a1fb1ede6850b4dc697fa14e1b028737
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-25 22:33:24 +0100

    creating the popover and form for the topic creation page

--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,32 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent
+} from '@nextui-org/react';
+
+import { createTopic } from '@/actions';
+
 export default function TopicCreateForm(){
-  return <div>TopicCreateForm</div>;
+  return (
+    <Popover placement='left'>
+      <PopoverTrigger>
+        <Button color='primary' variant='flat'>Create Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className='text-lg'>Create a Topic</h3>
+            <Input label='name' labelPlacement='outside' placeholder='Name' />
+
+            <Textarea label='description' labelPlacement='outside' placeholder='Describe your topic' />
+
+            <Button type='submit' color='primary' variant='flat'>Create</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  )
 }
