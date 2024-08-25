1748440d186b87e6ed1020366cfa3ff2349845df
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-25 22:49:27 +0100

    Installing zod form validator & creating a topicSchema for zod validation & checking result error message with flatten function

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,9 +1,21 @@
 'use server'
+import {z} from 'zod';
+
+const createTopicSchema = z.object({
+  name: z.string().min(3).regex(/^[a-z-]+$/, {message: 'name must be lowercase or dashes'}),
+  description: z.string().min(10),
+});
 
 export async function createTopic(formData: FormData){
 
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
 
-  const name = formData.get('name') as string;
-  const description = formData.get('description') as string;
+  });
+
+  if (!result.success){
+    console.log(result.error.flatten().fieldErrors);
+  }
 
 }
