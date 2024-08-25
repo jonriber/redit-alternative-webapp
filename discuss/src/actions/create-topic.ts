5d318d5077a7517bd7bd1b290e7f667240a63c09
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-25 22:36:53 +0100

    checking what data I am receiving from my submit form action inside my server action

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,6 +1,9 @@
 'use server'
 
-export async function createTopic(){
-  // TODO: revalidate homepage
+export async function createTopic(formData: FormData){
+
+
+  const name = formData.get('name') as string;
+  const description = formData.get('description') as string;
 
 }
--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -19,9 +19,9 @@ export default function TopicCreateForm(){
         <form action={createTopic}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className='text-lg'>Create a Topic</h3>
-            <Input label='name' labelPlacement='outside' placeholder='Name' />
+            <Input name='name' label='name' labelPlacement='outside' placeholder='Name' />
 
-            <Textarea label='description' labelPlacement='outside' placeholder='Describe your topic' />
+            <Textarea name='description' label='description' labelPlacement='outside' placeholder='Describe your topic' />
 
             <Button type='submit' color='primary' variant='flat'>Create</Button>
           </div>
