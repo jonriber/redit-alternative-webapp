1805f4a6102677f7635368998f51feab83c60000
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-09-05 11:34:28 +0100

    new approach for type definition with extra data

--- discuss/src/db/queries/posts.ts
+++ discuss/src/db/queries/posts.ts
@@ -1,15 +1,17 @@
 import type { Post } from '@prisma/client';
 import { db } from '@/db';
 
-export type PostWithData = (
-  Post & {
-    topic: { slug: string }
-    user: { name: string | null};
-    _count: { comments: number }
-  }
-)
+// export type PostWithData = (
+//   Post & {
+//     topic: { slug: string }
+//     user: { name: string | null};
+//     _count: { comments: number }
+//   }
+// )
 
-export function fetchPostByTopicSlug(slug:string): Promise<PostWithData[]> {
+export type PostWithData = Awaited<ReturnType<typeof fetchPostByTopicSlug>>[number]
+
+export function fetchPostByTopicSlug(slug:string) {
   return db.post.findMany({
     where: { topic: { slug }},
     include: {
