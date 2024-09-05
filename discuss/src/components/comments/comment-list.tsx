2dbc1fd7e7b9797b104ad8f57a8ab483a58fee88
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-09-05 12:03:47 +0100

    Comments List functionality & comments nested rendering component

--- discuss/src/components/comments/comment-list.tsx
+++ discuss/src/components/comments/comment-list.tsx
@@ -1,9 +1,11 @@
 import CommentShow from "@/components/comments/comment-show";
+import type { CommentWithAuthor } from "@/db/queries/comments";
+interface CommentListProps {
+  fetchData: () => Promise<CommentWithAuthor[]>;
+}
 
-interface CommentListProps {}
-
-// TODO: Get a list of comments from somewhere
-export default function CommentList({}: CommentListProps) {
+export default async function CommentList({fetchData}: CommentListProps) {
+  const comments = await fetchData();
   const topLevelComments = comments.filter(
     (comment) => comment.parentId === null
   );
